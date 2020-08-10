ab7fd9aba6aedfc76fc611009574e5555dda1c73
giorgiozannini <44499913+giorgiozannini@users.noreply.github.com>
2020-08-10 19:24:34 +0200

    Update RealNVP.py
    Good evening! We have made the changes you requested, if there is any other mistake or if it is not well formatted please let us know.
    About the dataset, we have looked for a more realistic one for the density estimation task; we found a dataset with multiple clusters that we think might more accurately represent a real-life scenario.
    Best regards,
    Sanna Daniele, Zannini Quirini Giorgio, Mandolini Giorgio Maria

--- RealNVP.py
+++ RealNVP.py
@@ -1,9 +1,9 @@
 """
 Title: Density estimation using Real NVP
-Author: [Mandolini Giorgio Maria](https://www.linkedin.com/in/giorgio-maria-mandolini-a2a1b71b4/), [Sanna Daniele](https://www.linkedin.com/in/daniele-sanna-338629bb/), [Zannini Quirini Giorgio](https://www.linkedin.com/in/giorgio-zannini-quirini-16ab181a0/)
-Date created: 2020/03/15
-Last modified: 2020/08/07
-Description: Estimating the density distribution from a toy dataset.
+Authors: [Mandolini Giorgio Maria](https://www.linkedin.com/in/giorgio-maria-mandolini-a2a1b71b4/), [Sanna Daniele](https://www.linkedin.com/in/daniele-sanna-338629bb/), [Zannini Quirini Giorgio](https://www.linkedin.com/in/giorgio-zannini-quirini-16ab181a0/)
+Date created: 2020/8/10
+Last modified: 2020/8/10
+Description: Estimating the density distribution of multi-cluster dataset.
 """
 
 """
@@ -18,6 +18,11 @@ likelihood principle, resorting to the "change of variable formula".
 An affine coupling layer function is built. It is created such that its inverse and the determinant
 of the jacobian is easy to obtain (more details in the referenced paper).
 
+requirements:
+
+* Tensorflow 2.3.0
+* Tensorflow probability 0.11.0
+
 References:
 [Density estimation using Real NVP](https://arxiv.org/pdf/1605.08803.pdf)
 """
@@ -27,24 +32,15 @@ References:
 
 """
 
-"""invisible
-pip install tensorflow-gpu==2.1.0rc0
-pip install tensorflow_probability==0.8.0rc0
-"""
-
-# Libraries to import
-
-import numpy as np
-import seaborn as sns
-import pandas as pd
-import matplotlib.pyplot as plt
+# libraries to import
 import tensorflow as tf
-from sklearn.datasets import make_moons
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import regularizers
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow_probability as tfp
-from tensorflow.keras.layers import Layer, Dense, ReLU, Activation, Input
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras import Model
-from tqdm import notebook
 
 """
 ## Load the data
@@ -52,11 +48,11 @@ from tqdm import notebook
 
 # Generating the examples
 
-moons = make_moons(1000, noise=0.05)[0].astype("float32")
-moons_df = pd.DataFrame(moons)
-plt.figure(figsize=(15, 10))
-moons_plot = sns.scatterplot(x=0, y=1, data=moons_df)
-moons_plot.set(title="Double moons", xlabel="x", ylabel="y")
+url = "http://cs.joensuu.fi/sipu/datasets/s1.txt"
+path = tf.keras.utils.get_file("dataset", url)
+data = np.loadtxt(path)
+scaler = StandardScaler()
+data = scaler.fit_transform(data)
 
 """
 ## Affine coupling layer
@@ -65,60 +61,55 @@ moons_plot.set(title="Double moons", xlabel="x", ylabel="y")
 # Creating a custom layer with keras API
 
 
-class s_t(Layer):
-    def __init__(
-        self, input_shape, nweights=256, reg=0.01, name="net", dtype="float32"
-    ):
-        super(s_t, self).__init__()
-
-        # t has no activation function
-        # s has tanh activation function
-        # all the other layers have a relu activation function
-
-        self.layer1 = Dense(nweights, activation="relu", kernel_regularizer=l2(reg))
-        self.layer2 = Dense(nweights, activation="relu", kernel_regularizer=l2(reg))
-        self.layer3 = Dense(nweights, activation="relu", kernel_regularizer=l2(reg))
-        self.layer4 = Dense(nweights, activation="relu", kernel_regularizer=l2(reg))
-        self.layer5 = Dense(
-            input_shape, activation="linear", kernel_regularizer=l2(reg)
-        )
-
-        self.layer6 = Dense(nweights, activation="relu", kernel_regularizer=l2(reg))
-        self.layer7 = Dense(nweights, activation="relu", kernel_regularizer=l2(reg))
-        self.layer8 = Dense(nweights, activation="relu", kernel_regularizer=l2(reg))
-        self.layer9 = Dense(nweights, activation="relu", kernel_regularizer=l2(reg))
-        self.layer10 = Dense(input_shape, activation="tanh", kernel_regularizer=l2(reg))
-
-    # Operations performed when the layer is called
-
-    def call(self, x):
-
-        # Calling the previously specified layers one by one
-        y = self.layer1(x)
-        y = self.layer2(y)
-        y = self.layer3(y)
-        y = self.layer4(y)
-        t = self.layer5(y)
-
-        z = self.layer6(x)
-        z = self.layer7(z)
-        z = self.layer8(z)
-        z = self.layer9(z)
-        s = self.layer10(z)
-
-        return s, t
+nweights = 256
+reg = 0.01
 
 
+def Coupling(input_shape):
+    input = keras.layers.Input(shape=input_shape)
+
+    t_layer_1 = keras.layers.Dense(
+        nweights, activation="relu", kernel_regularizer=regularizers.l2(reg)
+    )(input)
+    t_layer_2 = keras.layers.Dense(
+        nweights, activation="relu", kernel_regularizer=regularizers.l2(reg)
+    )(t_layer_1)
+    t_layer_3 = keras.layers.Dense(
+        nweights, activation="relu", kernel_regularizer=regularizers.l2(reg)
+    )(t_layer_2)
+    t_layer_4 = keras.layers.Dense(
+        nweights, activation="relu", kernel_regularizer=regularizers.l2(reg)
+    )(t_layer_3)
+    t_layer_5 = keras.layers.Dense(
+        2, activation="linear", kernel_regularizer=regularizers.l2(reg)
+    )(t_layer_4)
+
+    s_layer_1 = keras.layers.Dense(
+        nweights, activation="relu", kernel_regularizer=regularizers.l2(reg)
+    )(input)
+    s_layer_2 = keras.layers.Dense(
+        nweights, activation="relu", kernel_regularizer=regularizers.l2(reg)
+    )(s_layer_1)
+    s_layer_3 = keras.layers.Dense(
+        nweights, activation="relu", kernel_regularizer=regularizers.l2(reg)
+    )(s_layer_2)
+    s_layer_4 = keras.layers.Dense(
+        nweights, activation="relu", kernel_regularizer=regularizers.l2(reg)
+    )(s_layer_3)
+    s_layer_5 = keras.layers.Dense(
+        2, activation="tanh", kernel_regularizer=regularizers.l2(reg)
+    )(s_layer_4)
+    return keras.Model(inputs=input, outputs=[s_layer_5, t_layer_5])
+
+
+Coupling(2).summary()
 """
 ## Real NVP
 """
-
 # class containing the whole set of operations
-
-
-class realnvp(Layer):
-    def __init__(self, layers, masks, distr):
-        super(realnvp, self).__init__()
+class Realnvp(keras.Model):
+    def __init__(self, layers_list, masks, distr):
+        super(Realnvp, self).__init__()
 
         # number of coupling layers
         self.num_cl = len(masks)
@@ -127,10 +118,9 @@ class realnvp(Layer):
         # masks to divide into 1:d and d+1:D
         self.masks = masks
         # s and t custom layers of before
-        self.layers = layers
+        self.layers_list = layers_list
 
-    # Custom function defining the forward operation
-
+    # custom function defining the forward operation
     def forward(self, y):
         # log determinant of the forward pass
         log_det_for = tf.zeros(y.shape[0])
@@ -138,16 +128,15 @@ class realnvp(Layer):
         for i in range(self.num_cl):
             x_masked = x * self.masks[i]
             reversed_mask = 1 - self.masks[i]
-            s, t = self.layers[i](x_masked)
-            x = x_masked + reversed_mask * (
-                x * tf.exp(s) * reversed_mask + t * reversed_mask
-            )
+            s, t = self.layers_list[i](x_masked)
+            s *= reversed_mask
+            t *= reversed_mask
+            x = x_masked + reversed_mask * (x * tf.exp(s) + t)
             log_det_for += tf.reduce_sum(s, [1])
 
         return x, log_det_for
 
-    # Custom function defining the forward operation
-
+    # custom function defining the forward operation
     def inverse(self, x):
         # log determinant of the forward pass
         log_det_inv = tf.zeros(x.shape[0])
@@ -155,20 +144,33 @@ class realnvp(Layer):
         for i in reversed(range(self.num_cl)):
             y_masked = y * self.masks[i]
             reversed_mask = 1 - self.masks[i]
-            s, t = self.layers[i](y_masked)
-            y = (
-                reversed_mask * (y - t * reversed_mask) * tf.exp(-(s * reversed_mask))
-                + y_masked
-            )
+            s, t = self.layers_list[i](y_masked)
+            s *= reversed_mask
+            t *= reversed_mask
+            y = reversed_mask * (y - t) * tf.exp(-s) + y_masked
             log_det_inv -= tf.reduce_sum(s, [1])
 
         return y, log_det_inv
 
     # log likelihood of the normal distribution + the log determinant of the jacobian
-
     def log_likelihood(self, x):
         y, logdet = self.inverse(x)
         return self.distr.log_prob(y) + logdet
+
+    def log_loss(self, x):
+        return -tf.reduce_mean(self.log_likelihood(x))
+
+    def train_step(self, data):
+
+        with tf.GradientTape() as tape:
+            loss = self.log_loss(data)
+
+        # updating the variables
+        g = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(g, self.trainable_variables))
+        loss_tracker.update_state(loss)
+
+        return {"loss": loss_tracker.result()}
 
 
 """
@@ -178,68 +180,72 @@ class realnvp(Layer):
 # initializing the masks, the layers and the prior distribution for the real nvp
 num_cl = 6
 masks = np.array([[0, 1], [1, 0]] * (num_cl // 2), dtype="float32")
-layers = [s_t(2) for i in range(num_cl)]
+
+layers_list = [Coupling(2) for i in range(num_cl)]
 
 tfd = tfp.distributions
 distr = tfd.MultivariateNormalDiag(loc=[0.0, 0.0], scale_diag=[1.0, 1.0])
 
-real_nvp = realnvp(layers, masks, distr)
-
 """
 ## Model training
 """
+loss_tracker = keras.metrics.Mean(name="loss")
 
-loss_history = []
 # Adam optimizer
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.00005)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
 
-# 2000 epochs
-for i in notebook.tqdm(range(2000)):
-    with tf.GradientTape() as tape:
+model = Realnvp(layers_list, masks, distr)
 
-        # the loss is - the mean of the log likelihood
-        loss = -tf.reduce_mean(real_nvp.log_likelihood(moons))
-        loss_history.append(loss)
-        if i % 100 == 0:
-            print(loss)
+model.compile(optimizer=optimizer)
 
-    # updating the variables
-    g = tape.gradient(loss, real_nvp.trainable_variables)
-    l = optimizer.apply_gradients(zip(g, real_nvp.trainable_variables))
 
+class Loss_every_100_epochs(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if epoch % 500 == 0:
+            print(epoch, "loss", round(logs.get("loss"), 4))
+
+
+# Training model
+history = model.fit(
+    data,
+    batch_size=len(data),
+    epochs=7500,
+    callbacks=[Loss_every_100_epochs()],
+    verbose=0,
+)
 """
 ## Performance evaluation
 """
 
-# loss history
-sns.set_style("whitegrid")
 plt.figure(figsize=(15, 10))
-ax = sns.lineplot(range(len(loss_history)), np.array(loss_history))
-ax.set(title="Loss history", xlabel="Epochs", ylabel="Loss")
+plt.plot(history.history["loss"])
+plt.title("model loss")
+plt.ylabel("loss")
+plt.xlabel("epoch")
 
-# From moons to latent space
-z, _ = real_nvp.inverse(moons)
+# from data to latent space
+z, _ = model.inverse(data)
 
-# From latent space to moons
-samples = distr.sample(100)
-x, _ = real_nvp.forward(samples)
-
-# Storing variables into dataframe to plot with seaborn
-moons_df, z_df, samples_df, x_df = (
-    pd.DataFrame(moons),
-    pd.DataFrame(np.array(z)),
-    pd.DataFrame(np.array(samples)),
-    pd.DataFrame(np.array(x)),
-)
+# from latent space to data
+samples = distr.sample(3000)
+x, _ = model.forward(samples)
 
 f, axes = plt.subplots(2, 2)
 f.set_size_inches(20, 15)
 
-ax_0 = sns.scatterplot(x=0, y=1, data=moons_df, ax=axes[0, 0])
-ax_0.set(title="Inference data space X", xlabel="x", ylabel="y")
-ax_1 = sns.scatterplot(x=0, y=1, data=z_df, ax=axes[0, 1])
-ax_1.set(title="Inference latent space Z", xlabel="x", ylabel="y")
-ax_2 = sns.scatterplot(x=0, y=1, data=samples_df, ax=axes[1, 0], color="g")
-ax_2.set(title="Generated latent space Z", xlabel="x", ylabel="y")
-ax_3 = sns.scatterplot(x=0, y=1, data=x_df, ax=axes[1, 1], color="g")
-ax_3.set(title="Generated data space X", label="x", ylabel="y")
+axes[0, 0].scatter(data[:, 0], data[:, 1], color="r")
+axes[0, 0].set(title="Inference data space X", xlabel="x", ylabel="y")
+axes[0, 0].set_xlim([-2.3, 2])
+axes[0, 0].set_ylim([-2, 2.3])
+axes[0, 1].scatter(z[:, 0], z[:, 1], color="r")
+axes[0, 1].set(title="Inference latent space Z", xlabel="x", ylabel="y")
+axes[0, 1].set_xlim([-3.5, 4])
+axes[0, 1].set_ylim([-4, 4])
+axes[1, 0].scatter(samples[:, 0], samples[:, 1], color="g")
+axes[1, 0].set(title="Generated latent space Z", xlabel="x", ylabel="y")
+axes[1, 0].set_xlim([-3.5, 4])
+axes[1, 0].set_ylim([-4, 4])
+axes[1, 1].scatter(x[:, 0], x[:, 1], color="g")
+axes[1, 1].set(title="Generated data space X", label="x", ylabel="y")
+axes[1, 1].set_xlim([-2.3, 2])
+axes[1, 1].set_ylim([-2, 2.3])
